f1c73ab13c51ed3ffdce2b530acad1e1cfb05f69
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-08-17 11:49:24 -0300

    Add files via upload

---
+++ PSA/app/control/CRUDCategoryTest.java
@@ -0,0 +1,37 @@
+package app.control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CRUDCategoryTest implements CRUDCategoryInterface{
+    private List<String> list;
+    
+    public CRUDCategoryTest() {
+        list = new ArrayList<>();
+        setList("Thomas", "Turbando", "Oscar", "Alho", "Isadora", "Pinto", "Jacinto", "Leitte", "Aquino", "Rego", "Paula", "Tejando", "Ana", "Konda", "Cuca", "Beludo",
+        "Diva", "Aginaberta", "Tati", "Komenno", "Rafaela");
+    }
+    
+    private void setList(String... args)
+    {
+        for(String a : args) {
+            list.add(a);
+        }
+    }
+    
+    @Override
+    public List<String> getAllCategories() {
+        return list;
+    }
+
+    @Override
+    public String getCategoryById(int id) {
+        return list.get(id);
+    }
+    
+    @Override
+    public void deleteCategoryById(int id) {
+        list.remove(id);
+    }
+    
+}
