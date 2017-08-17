030c090201cfc9afe1000886f7e6fe5ac687694f
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-08-17 16:57:17 -0300

    Delete CRUDCategoryInterface.java

--- PSA/app/control/CRUDCategoryInterface.java
+++
@@ -1,9 +0,0 @@
-package app.control;
-
-import java.util.List;
-
-public interface CRUDCategoryInterface {
-    public List<String> getAllCategories();
-    public String getCategoryById(int id);
-    public void deleteCategoryById(int id);
-}
