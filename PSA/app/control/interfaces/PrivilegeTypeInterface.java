4f1f5aada5e9764d74189a232fd61e25cd68cc7b
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-08-17 16:58:03 -0300

    Add files via upload

---
+++ PSA/app/control/interfaces/PrivilegeTypeInterface.java
@@ -0,0 +1,6 @@
+package app.control.interfaces;
+
+public interface PrivilegeTypeInterface {
+    public int getPrivilegeType();
+    public void setPrivilegeType(int type);
+}
---
+++ PSA/app/control/interfaces/PrivilegiesButtonsInterface.java
@@ -0,0 +1,7 @@
+package app.control.interfaces;
+
+import java.util.List;
+
+public interface PrivilegiesButtonsInterface extends PrivilegeTypeInterface{
+    public List<String> getButtons();
+}
