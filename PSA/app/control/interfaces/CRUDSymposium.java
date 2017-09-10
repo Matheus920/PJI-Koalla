f1f6bdf3d52cc17e31c4615e31b7fbabbca6c79a
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-09-10 12:21:09 -0300

    Delete CRUDSymposium.java

--- PSA/app/control/interfaces/CRUDSymposium.java
+++
@@ -1,9 +0,0 @@
-package app.control.interfaces;
-
-import java.util.List;
-
-public interface CRUDSymposium {
-    public String getOverview();
-    public List<String> getSpeakers();
-    public String getPlace();
-}
