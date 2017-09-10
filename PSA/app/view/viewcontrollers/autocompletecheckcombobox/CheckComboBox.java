a78aa28908969c80e7cfc4eff5f949e05b07d8b8
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-09-10 14:09:27 -0300

    Add files via upload

---
+++ PSA/app/view/viewcontrollers/autocompletecheckcombobox/CheckComboBox.java
@@ -0,0 +1,68 @@
+package app.autocompletecheckcombobox;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.CheckBox;
+
+public class CheckComboBox<T extends CheckString> extends ComboBox<CheckBox> {
+    private ObservableList<T> checkItems;
+    private ObservableList<T> selectedItems;
+    private Map<T, Integer> itemsMap;
+    
+    public CheckComboBox(ObservableList<T> items) {
+        super();
+        this.checkItems = FXCollections.<T>observableArrayList(items);
+        this.selectedItems = FXCollections.<T>observableArrayList();
+        ObservableList<CheckBox> x = FXCollections.<CheckBox>observableArrayList();
+        itemsMap = new HashMap<T, Integer>();
+        for(T a : checkItems) {
+            CheckBox check = new CheckBox(a.checkName());
+            check.selectedProperty().addListener(new ChangeListener<Boolean>(){
+                @Override
+                public void changed(ObservableValue<? extends Boolean> obv, Boolean ov, Boolean nv) {
+                    if(nv) {
+                        itemsMap.put(a, checkItems.indexOf(a));
+                        List<Integer> temp = new ArrayList<>();
+                        temp.addAll(itemsMap.values());
+                        Collections.sort(temp);
+                        selectedItems.clear();
+                        for(Integer a : temp) {
+                            selectedItems.add(checkItems.get(a));
+                        }
+                    } else {
+                        itemsMap.remove(a);
+                        List<Integer> temp = new ArrayList<>();
+                        temp.addAll(itemsMap.values());
+                        Collections.sort(temp);
+                        selectedItems.clear();
+                        for(Integer a : temp) {
+                            selectedItems.add(checkItems.get(a));
+                        }
+                    }
+                }
+            });
+            x.add(check);
+        }
+        super.setItems(x);
+        new AutoCompleteComboBox<CheckBox>().bindAutoComplete(this);
+    }
+
+    public ObservableList<T> getCheckItems() {
+        return checkItems;
+    }
+    
+    public ObservableList<T> getSelectedItems() {
+        return selectedItems;
+    }
+    
+    
+}
