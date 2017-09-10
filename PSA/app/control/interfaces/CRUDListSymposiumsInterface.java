987a19b60678a7cb67c9f55e7d8f6018dadf8b88
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-09-10 12:21:03 -0300

    Delete CRUDListSymposiumsInterface.java

--- PSA/app/control/interfaces/CRUDListSymposiumsInterface.java
+++
@@ -1,9 +0,0 @@
-package app.control.interfaces;
-
-import java.util.List;
-
-public interface CRUDListSymposiumsInterface {
-    public List<String[]> listSymposium();
-    public List<String[]> listSymposium(int page);
-    public int numberPages();
-}
