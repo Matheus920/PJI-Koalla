36af4e3947da52b29d184212e51bd864fd16b74d
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-09-27 18:20:50 -0300

    Add files via upload

--- PSA/app/view/AddEventView.java
+++ PSA/app/view/AddEventView.java
@@ -1,31 +1,37 @@
 package app.view;
 
+import app.view.viewcontrollers.MaskField;
 import app.Main;
 import app.control.interfaces.CRUDCriteriaInterface;
 import app.control.interfaces.CRUDEvaluatorInterface;
-import app.view.viewcontrollers.MaskField;
 import app.view.viewcontrollers.autocompletecheckcombobox.CheckComboBox;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class AddEventView {
     private Stage stage;
+    private final ScrollPane scrollPane = new ScrollPane();
+    private final GridPane gridPane = new GridPane();
     private final CRUDEvaluatorInterface evaluators;
     private final CRUDCriteriaInterface criteria;
     
@@ -37,8 +43,6 @@ public class AddEventView {
     }
     
     private void eventPageOne() {
-        ScrollPane scrollPane = new ScrollPane();
-        GridPane gridPane = new GridPane();
         stage.setTitle("Inserir dados do evento");
         Label lblTitle = new Label("Título:");
         TextField title = new TextField();
@@ -52,7 +56,6 @@ public class AddEventView {
         Label lblDuration = new Label("Duração:");
         TextField duration = new TextField();
         Button btnNext = new Button("Avançar");
-
         btnNext.setOnAction(e -> {
             eventPageTwo();
         });
@@ -117,78 +120,45 @@ public class AddEventView {
         scrollPane.setContent(gridPane);
         
         Main.setBackgroundWhite(gridPane, hbox1, scrollPane);
-        Scene scene = new Scene(scrollPane, 730, 600);
-        stage.setScene(scene);
-    }
-
-    private String addEvaluatorView() {
-        StackPane stackPane = new StackPane();
-        ListView<String> list = new ListView<String>(FXCollections.observableArrayList(evaluators.getAllEvaluators()));
-        stackPane.getChildren().addAll(list);
-        final String[] result = {""};
-
-        Scene scene = new Scene(stackPane, 400, 500);
-        Stage newStage = new Stage();
-        newStage.setScene(scene);
-        newStage.initModality(Modality.APPLICATION_MODAL);
-        newStage.setResizable(false);
-
-        list.setOnMouseClicked(e -> {
-            //System.out.println("Rola");
-            result[0] = list.getSelectionModel().getSelectedItem();
-            newStage.close();
-        });
-
-        newStage.showAndWait();
-        return result[0];
     }
     
     private void eventPageTwo(){
-        StackPane stackPane = new StackPane();
         stage.setTitle("Defina os avaliadores do projeto");
         
         
         Button addEvaluator = new Button("*");
         Button addCriteria = new Button("*");
-
+        
         Button next = new Button("Avançar");
         Button cancel = new Button("Cancelar");
-
-        next.setCursor(Cursor.HAND);
-        cancel.setCursor(Cursor.HAND);
-
+        
+        
         Label lblEvaluators = new Label("Lista de avaliadores:");
-        ObservableList<String> data = FXCollections.observableArrayList(evaluators.getAllEvaluators());
-        CheckComboBox<String> ccbEvaluators = new CheckComboBox(data, "Avaliadores");
+        CheckComboBox<String> ccbEvaluators = new CheckComboBox(FXCollections.observableArrayList(evaluators.getAllEvaluators()), "Avaliadores");
 
         Label lblSelectedEvaluators = new Label("Avaliadores selecionados:");
         FlowPane flowPaneEvaluators = new FlowPane();
-
-        addEvaluator.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                ccbEvaluators.addCheckItem(addEvaluatorView());
-            }
-        });
-
-        for(CheckBox selectedEvaluator : ccbEvaluators.getItems()){
+        
+         for(CheckBox selectedEvaluator : ccbEvaluators.getItems()){
             selectedEvaluator.selectedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
-                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                    if(newValue){
-                        Label a = new Label((char)0x2022 + selectedEvaluator.getText());
-                        flowPaneEvaluators.getChildren().add(a);
-                    }else {
-                        for(int i = 0; i < flowPaneEvaluators.getChildren().size(); i++) {
-                            if(((Label)flowPaneEvaluators.getChildren().get(i)).getText().contains(selectedEvaluator.getText())) {
-                                flowPaneEvaluators.getChildren().remove(i);
-                            }
-                        }
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue){
+                Label a = new Label((char)0x2022 + selectedEvaluator.getText());
+                flowPaneEvaluators.getChildren().add(a);
+            }else {
+                for(int i = 0; i < flowPaneEvaluators.getChildren().size(); i++) {
+                    if(((Label)flowPaneEvaluators.getChildren().get(i)).getText().contains(selectedEvaluator.getText())) {
+                        flowPaneEvaluators.getChildren().remove(i);
                     }
                 }
-            });
+            }
+    
+            }
+        });
         }
-
+            
+        
         Label lblCriteria = new Label("Lista de critérios:");
         CheckComboBox<String> ccbCriteria = new CheckComboBox(FXCollections.observableArrayList(criteria.getAllCriteria()), "Critérios");
         Label lblSelectedCriteria = new Label("Critérios selecionados:");
@@ -222,18 +192,15 @@ public class AddEventView {
         flowPaneCriteria.setVgap(10);
         flowPaneCriteria.setOrientation(Orientation.VERTICAL);
         flowPaneCriteria.setPrefWrapLength(150);
-
+     
         HBox hbox1 = new HBox(lblEvaluators, ccbEvaluators, addEvaluator);
         hbox1.setSpacing(10);
-
-        hbox1.setMargin(lblEvaluators, new Insets(0, 0, 0, 5));
-        hbox1.setMargin(ccbEvaluators, new Insets(0, 0, 0, 0));
-
-        ccbEvaluators.setPrefWidth(130);
+        
+        hbox1.setMargin(lblEvaluators, new Insets(5, 0, 0, 5));
+        hbox1.setMargin(ccbEvaluators, new Insets(5, 0, 0, 0));
+        
 
         HBox hbox2 = new HBox(lblCriteria, ccbCriteria, addCriteria);
-        ccbCriteria.setPrefWidth(lengthBiggestString(ccbCriteria.getCheckItems()) * 7);
-
         hbox2.setSpacing(10);
         
         next.setOnAction(e-> {
@@ -242,40 +209,18 @@ public class AddEventView {
         
         HBox hbox3 = new HBox(next, cancel);
         hbox3.setSpacing(10);
-
-        hbox1.setAlignment(Pos.TOP_CENTER);
-        hbox2.setAlignment(Pos.TOP_CENTER);
-        hbox3.setAlignment(Pos.BOTTOM_RIGHT);
-        hbox3.setPadding(new Insets(20, 0, 0, 0));
-        flowPaneCriteria.setAlignment(Pos.CENTER);
-        flowPaneEvaluators.setAlignment(Pos.CENTER);
-
-
-        flowPaneCriteria.setBorder(new Border(new BorderStroke(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
-                BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID,
-                CornerRadii.EMPTY, new BorderWidths(1), Insets.EMPTY)));
-
-        flowPaneEvaluators.setBorder(new Border(new BorderStroke(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
-                BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID,
-                CornerRadii.EMPTY, new BorderWidths(1), Insets.EMPTY)));
-
+        
         VBox vbox1 = new VBox(hbox1, lblSelectedEvaluators, flowPaneEvaluators, hbox2, lblSelectedCriteria,
         flowPaneCriteria, hbox3);
-
+        
         vbox1.setSpacing(10);
-
+        
+        //TODO: pintar de branco
         Main.setBackgroundWhite(vbox1);
-        Main.setBackgroundWhite(stackPane);
-
-        vbox1.setAlignment(Pos.TOP_CENTER);
-        vbox1.setPadding(new Insets(10, 0, 0, 0));
-        stackPane.getChildren().add(vbox1);
-        Scene scene = new Scene(stackPane, 730, 600);
-        stage.setScene(scene);
+        scrollPane.setContent(vbox1);
     }
     
     public void eventPageThree(){
-        ScrollPane scrollPane = new ScrollPane();
         stage.setTitle("Defina informações sobre local e palestrantes");
         Label lblPlace = new Label("Local: ");
         TextField tfPlace = new TextField();
@@ -328,26 +273,16 @@ public class AddEventView {
         });
         
         scrollPane.setContent(vbox);
-        Main.setBackgroundWhite(scrollPane);
-        Scene scene = new Scene(scrollPane, 730, 600);
-        stage.setScene(scene);
+        
     }
     
     public void addEventShow() {
-       // Scene scene = new Scene(anchorPane, 730, 600);
-        //stage.setScene(scene);
+        Scene scene = new Scene(scrollPane, 730, 600);
+        stage.setScene(scene);
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setResizable(false);
         stage.showAndWait();
     }
     
-    public int lengthBiggestString(ObservableList<String> data) {
-        int max = 0;
-        for(String a : data) {
-            if(a.length() > max) {
-                max = a.length();
-            }
-        }
-        return max;
-    }
+   
 }
