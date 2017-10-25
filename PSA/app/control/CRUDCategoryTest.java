2f63322676981d55c55f9ea3fb7d24f53f28da2f
Matheus920 <matheusm920@gmail.com>
2017-10-25 18:00:11 -0200

    Add files via upload

--- PSA/app/control/CRUDCategoryTest.java
+++ PSA/app/control/CRUDCategoryTest.java
@@ -7,30 +7,38 @@ import java.util.List;
 
 public class CRUDCategoryTest implements CRUDCategoryInterface{
 
-    @Override
-    public Category addCategory(Category category) {
-       return new CategoryDAO().addCategory(category);
-    }
+@Override
+public Category addCategory(Category category) {
+   return new CategoryDAO().addCategory(category);
+}
 
-    @Override
-    public void updateCategory(Category category) {
-        new CategoryDAO().updateCategory(category);
-    }
+@Override
+public void updateCategory(Category category) {
+    new CategoryDAO().updateCategory(category);
+}
 
-    @Override
-    public void deleteCategory(Category category) {
-        new CategoryDAO().deleteCategory(category);
-    }
+@Override
+public void deleteCategory(Category category) {
+    new CategoryDAO().deleteCategory(category);
+}
 
-    @Override
-    public Category getCategoryById(long id) {
-        return new CategoryDAO().getCategoryById(id);
-    }
+@Override
+public Category getCategoryById(long id) {
+    return new CategoryDAO().getCategoryById(id);
+}
 
-    @Override
-    public List<Category> getAllCategories() {
-        return new CategoryDAO().getAllCategories();
-    }
+@Override
+public List<Category> getAllCategories() {
+    return new CategoryDAO().getAllCategories();
+}
 
-    
+@Override
+public boolean exists(String name){
+    return new CategoryDAO().exists(name);
+}
+
+@Override
+public List<String> getAllCategoriesNames() {
+    return new CategoryDAO().getAllCategoriesNames();
+}
 }
